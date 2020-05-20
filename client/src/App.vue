42e62c30c4216a7e81542421a0c4461cab52c813
mark <markpackham1@gmail.com>
2020-05-20 15:54:02 +0100

    sort out guest location methods issue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,27 +2,28 @@
   <div id="app">
     <main-header></main-header>
     <guest-form></guest-form>
-    <div v-for="(guest ,index) in guests" :guest="guest" :key="index">
-      <ul>
-        <li>{{ guest.name }}</li>
-        <li>{{ guest.email }}</li>
-        <li>{{ guest.checked_in }}</li>
-      </ul>
-    </div>
+    <ul v-for="(guest ,index) in guests" :guest="guest" :key="index">
+      <li>{{ guest.name }}</li>
+      <li>{{ guest.email }}</li>
+      <li>{{ guest.checked_in }}</li>
+      <button v-on:click="deleteGuest">Delete booking</button>
+    </ul>
     <main-footer></main-footer>
   </div>
 </template>
 
 <script>
+import { eventBus } from "./main.js";
 import GuestService from "./services/GuestService.js";
 import MainHeader from "./components/layouts/Main-Header";
 import MainFooter from "./components/layouts/Main-Footer";
-import GuestForm from './components/GuestForm';
+import GuestForm from "./components/GuestForm";
 export default {
   name: "app",
   data() {
     return {
-      guests: []
+      guests: [],
+      guest: {}
     };
   },
   components: {
@@ -34,10 +35,25 @@ export default {
     fetchData() {
       GuestService.getGuests().then(guests => (this.guests = guests));
     },
-    
+    deleteGuest() {
+      console.log(this.guest);
+      GuestService.deleteGuest(this.guest._id).then(() =>
+        eventBus.$emit("guest-deleted", this.guest._id)
+      );
+    }
   },
   mounted() {
     this.fetchData();
+
+    // eventBus.$on("guest-deleted", id => {
+    //   GuestService.deleteGuest(id);
+    //   const index = this.guests.findIndex(guest => guest._id === id);
+    //   this.guests.splice(index, 1);
+    // });
+    eventBus.$on("guest-deleted", id => {
+      let index = this.guests.findIndex(guest => guest._id === id);
+      this.guests.splice(index, 1);
+    });
   }
 };
 </script>
