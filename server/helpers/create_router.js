2cb20f1e3e0c02259385c180befd1fd62ba230a9
Rich <richgrant192@gmail.com>
2020-05-20 13:55:42 +0100

    added index route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,10 +1,24 @@
-const express = require("express")
-const ObjectID = require("mongodb").ObjectID;
+const express = require('express')
+const ObjectID = require('mongodb').ObjectID;
 
 const createRouter = function (collection){
     const router = express.Router();
 
     // INDEX
+
+    router.get('/', (req, res) => {
+        collection
+        .find()
+        .toArray()
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error, err });
+
+            
+        });
+    });
 
 
     // CREATE
