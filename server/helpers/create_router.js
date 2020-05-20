ba8a49044ab754dbdc496e297ce28347ec6a41fe
mark <markpackham1@gmail.com>
2020-05-20 12:09:24 +0100

    Setup Database connections

---
+++ server/helpers/create_router.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const ObjectID = require("mongodb").ObjectID;
+
+const createRouter = function (collection){
+    const router = express.Router();
+
+    // INDEX
+
+
+    // CREATE
+
+
+    // DELETE
+
+    
+    return router;
+
+    // Not used: SHOW or UPDATE
+}
+
+module.exports = createRouter;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js')
+
+app.use(parser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+    .then((client) => {
+        const db = client.db('hotel');
+        const hotelBookings = db.collection('guest');
+        const guestRouter = createRouter(hotelBookings);
+        app.use('/api/bookings/', guestRouter);
+    })
+      .catch(console.error);
+
+app.listen(3000, function () {
+    console.log(`Listening on port ${ this.address().port }`);
+});
+    
